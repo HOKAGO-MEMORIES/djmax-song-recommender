4b298ee5e0bd89be77d21dc4770019b3ff9945ba
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-11-12 04:33:40 +0900

    test(JsonParser): JSON 파일을 불러와 List<Song> 객체를 생성하고 마지막 곡을 확인하는 테스트 추가

--- src/test/java/util/JsonParserTest.java
+++ src/test/java/util/JsonParserTest.java
@@ -6,20 +6,36 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class JsonParserTest {
 
-    @Test
-    @DisplayName("곡 목록 JSON 파일을 불러와 songs 객체를 생성한다.")
-    void parse_jsonToSong() {
+    private List<Song> songs;
+
+    @BeforeEach
+    void setup() {
         String jsonString = loadJson("sample_songs.json");
+        this.songs = JsonParser.parseSongs(jsonString);
+    }
 
-        List<Song> songs = JsonParser.parseSongs(jsonString);
-
+    @Test
+    @DisplayName("곡 목록 JSON 파일을 불러와 List<Song> 객체를 생성한다.")
+    void parse_jsonToSong() {
         Assertions.assertThat(songs).isNotEmpty();
         Assertions.assertThat(songs.getFirst().name()).isEqualTo("비상 ~Stay With Me~");
+    }
+
+    @Test
+    @DisplayName("곡 목록 JSON 파일을 불러와 List<Song> 객체를 생성하고 파일 마지막 곡을 확인한다.")
+    void parse_jsonToSong_check_lastSong() {
+        Assertions.assertThat(songs).isNotEmpty();
+        Assertions.assertThat(songs.getLast().name()).isEqualTo("AURORA");
+        Assertions.assertThat(songs.getLast().composer()).isEqualTo("DIEIN");
+        Assertions.assertThat(songs.getLast().patterns().b4().HD().level()).isEqualTo(6);
+        Assertions.assertThat(songs.getLast().patterns().b6().SC().floor()).isEqualTo(4.1);
+        Assertions.assertThat(songs.getLast().patterns().b8().NM().floor()).isNull();
     }
 
     private String loadJson(String fileName) {
