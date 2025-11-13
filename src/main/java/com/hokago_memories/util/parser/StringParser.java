17e32358da3f104a256b5e6bce15bcc8f5fbbf0d
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-11-13 21:38:08 +0900

    feat(StringParser): String을 int로 파싱하는 기능 추가

---
+++ src/main/java/com/hokago_memories/util/parser/StringParser.java
@@ -0,0 +1,11 @@
+package com.hokago_memories.util.parser;
+
+public class StringParser {
+
+    private StringParser() {
+    }
+
+    public static int ParseStringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
