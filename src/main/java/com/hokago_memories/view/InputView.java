e39ca76f1011e12350f251cf886c5095a95583bd
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-11-13 20:59:41 +0900

    feat(InputView, Console): 사용자의 입력을 받는 기능 추가

---
+++ src/main/java/com/hokago_memories/view/InputView.java
@@ -0,0 +1,10 @@
+package com.hokago_memories.view;
+
+import com.hokago_memories.util.Console;
+
+public class InputView {
+
+    public String readInput() {
+        return Console.readLine();
+    }
+}
