bcd9e9a8fcd29f6bb16518eafae963569eebd66d
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-11-16 22:15:52 +0900

    feat(PlayerInfoService): djClassCalculator에 DJCLASS를 계산하는 책임 위임

--- src/main/java/com/hokago_memories/service/PlayerInfoService.java
+++ src/main/java/com/hokago_memories/service/PlayerInfoService.java
@@ -1,17 +1,27 @@
 package com.hokago_memories.service;
 
+import com.hokago_memories.domain.DjClass;
+import com.hokago_memories.domain.PlayRecordDto;
 import com.hokago_memories.domain.Tier;
 import com.hokago_memories.domain.UserRequest;
+import java.util.List;
 
 public class PlayerInfoService {
 
     private final OpenApiService openApiService;
+    private final DjClassCalculator djClassCalculator;
 
-    public PlayerInfoService(OpenApiService openApiService) {
+    public PlayerInfoService(OpenApiService openApiService, DjClassCalculator djClassCalculator) {
         this.openApiService = openApiService;
+        this.djClassCalculator = djClassCalculator;
     }
 
     public Tier getUserTier(UserRequest request) {
         return openApiService.findTier(request.nickname(), request.button());
+    }
+
+    public DjClass getDjClass(UserRequest request) {
+        List<PlayRecordDto> records = openApiService.findDjClassBoard(request.nickname(), request.button());
+        return djClassCalculator.calculate(records, request.button());
     }
 }
